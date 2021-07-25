94b4c9543d63ca27dfae73e6c2a5dd7f09e289da
christinem <cmaynard@cvent.com>
2021-07-24 23:25:40 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(PORT, () => console.log(`Listening on port:${PORT}`));
