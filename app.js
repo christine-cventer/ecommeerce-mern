05f7aa147ced3b1165ba1c47ed7a05a5a0f3b112
Christinekrm02 <christinekrm02@gmail.com>
2021-07-25 00:26:04 -0400

    First commit

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+//db connection
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: true,
+    })
+    .then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', (err) => {
+    console.log(`DB connection error: ${err.message}`);
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(PORT, () => console.log(`Listening on port:${PORT}`));
