a42da86ee1ed3cea4b9b9817d6275838f1b6c5e3
Christine Maynard <christine.ri.maynard@gmail.com>
2021-12-12 21:40:24 -0500

    Add queries to ProductList

--- controllers/Products/ProductsSoldController.js
+++ controllers/Products/ProductsSoldController.js
@@ -3,38 +3,24 @@ import Product from "../../models/Product.js";
 
 /**
  * sell/arrival
- * retun by sell =/products?sortBy=sold&order=desc&limit=4
- * retun by arrival =/products?sortBy=createdAt&order=desc&limit=4
+ * retun by sell =/productSold?sortBy=sold&order=desc&limit=4
+ * retun by arrival =/productSold?sortBy=createdAt&order=desc&limit=4
  * all products are returned if no params are sent
  */
 
-// export default function ProductList(req, res, next) {
-//   // try {
-//   console.log(req);
-//   let order = req.query.order ? req.query.order : "asc";
-//   let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
-//   let limit = req.query.limit ? req.query.limit : 6;
-//   // TODO - make another req to fetch product photo
-//   const products = Product.find();
-//   // .select("-file")
-//   // .populate("ProductCategory")
-//   // .sort([[sortBy, order]])
-//   // .limit(limit)
-//   // .exec();
-
-//   return res.json({ msg: "data", products });
-//   // } catch (error) {
-//   //   return res.json({
-//   //     msg: "Error fetching product(s)",
-//   //     error: error.toString(),
-//   //   });
-//   // }
-//   next();
-// }
 export default async function ProductList(req, res) {
   try {
-    const products = await Product.find().populate("ProductCategory");
-    return res.json({ msg: "Found products", products });
+    let order = req.query.order ? req.query.order : "asc";
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+    const products = await Product.find()
+      .populate("ProductCategory")
+      .sort([[sortBy, order]])
+      .limit(limit)
+      .exec();
+    !products
+      ? res.json("No products found")
+      : res.json({ msg: "Found products", products });
   } catch (err) {
     return res.json({
       msg: err.Message,
