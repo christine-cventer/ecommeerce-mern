2e5195fcf1bda1810db939ebb103f13af5236004
Christine Maynard <christine.ri.maynard@gmail.com>
2021-12-24 22:15:21 -0500

    Add user signup form

--- src/pages/user/UserSignUp.jsx
+++ src/pages/user/UserSignUp.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../Layout";
-import env from "react-dotenv";
+// import env from "react-dotenv";
 
-const SignUp = () => <Layout title="Register Page">{env.API_URL}</Layout>;
+const SignUp = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+  const handleChange = (valueType) => (event) => {
+    setValues({ ...values, error: false, [valueType]: event.target.value });
+  };
+  return (
+    {
+      /* let's create a 12 column grid */
+    },
+    (
+      <Layout title="Register Page" className="container col-md-8 offset-md-2">
+        <form action="#">
+          <div className="form-group">
+            <label className="text-muted">Name</label>
+            <input
+              onChange={handleChange("name")}
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label className="text-muted">Email</label>
+            <input
+              onChange={handleChange("email")}
+              type="email"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label className="text-muted">Password</label>
+            <input
+              onChange={handleChange("password")}
+              type="password"
+              className="form-control"
+            />
+          </div>
+          <button className="btn btn-primary">Submit</button>
+        </form>
+        {/* {JSON.stringify(values)} */}
+      </Layout>
+    )
+  );
+};
 
 export default SignUp;
