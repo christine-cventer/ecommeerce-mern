5205e880e7572e1563a15f84c40963b8c17f90f1
Christine Maynard <christine.ri.maynard@gmail.com>
2022-12-04 18:48:09 -0500

    commit

---
+++ models/docModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const schema = Schema({
+  name: String,
+  email: String,
+  created: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('User', schema);
